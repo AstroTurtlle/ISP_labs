5fa10b88f430d8783348ac99229b994bcbb54f04
AstroTurtlle <117024926+AstroTurtlle@users.noreply.github.com>
2023-04-15 15:37:00 +0300

    Delete Semnale si Sisteme/Laborator 2 directory

--- Semnale si Sisteme/Laborator 2/SemnalExponentialDiscret.m
+++
@@ -1,9 +0,0 @@
-a = 0.2; b = 0.5;
-n1=0;n2=10;n=n1:n2;
-xe=a*b.^n;
-stem(n,xe);
-xlabel('n'); ylabel('x_e[n]'); title('x_e[n] = ab^n');
-
-xe=@(n)(a*b.^n);
-stem(n,xe(n));
-xlabel('n'); ylabel('x_e[n]'); title('x_e[n] = ab^n fplot');
--- Semnale si Sisteme/Laborator 2/imshowExemplu.m
+++
@@ -1,4 +0,0 @@
-I = imread('peppers.png'); I=rgb2gray(I);
-figure, imshow(I);
-[x,y] = meshgrid(1:size(I,2) , 1:size(I,1));
-figure, surf(x,y,double(I)), shading flat, colormap gray
